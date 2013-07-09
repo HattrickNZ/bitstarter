3867b968a0dbf456be0ed4d07480f1dd64dd4103
HattrickNZ <youngkevinsmith@hotmail.com>
2013-07-09 09:10:15 +0000

    Updated 2 files web and index

--- web.js
+++ web.js
@@ -1,9 +1,23 @@
 var express = require('express');
-
+var fs = require('fs')
 var app = express.createServer(express.logger());
+var content;
+var file = 'index.html';
+
+// First I want to read the file
+fs.readFile(file, 'utf8', function read(err, data) {
+    if (err) {
+        throw err;
+    }
+    content = data;
+});
+
+function processFile() {
+    return content;
+}
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(processFile());
 });
 
 var port = process.env.PORT || 5000;
